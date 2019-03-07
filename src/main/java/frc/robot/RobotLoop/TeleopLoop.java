83917b1e65dca875a08c9741f187bf69ded27159
Duong <duongnguyenthai78@yahoo.com>
2019-03-07 11:00:13 +0700

    Update

--- src/main/java/frc/robot/RobotLoop/TeleopLoop.java
+++ src/main/java/frc/robot/RobotLoop/TeleopLoop.java
@@ -18,22 +18,26 @@ public class TeleopLoop {
     public static void teleopLoop(){
         RobotMap.mManipulator.updateSensors();
 
-        if (Controls.TEST.uniquePress()){
+        //Change state on control
+        if (Controls.TEST.get()){
             StateManager.cargoState = CARGOSTATE.IN;
+        } 
+        else if (Controls.joystick.getRawButton(2)){
+            StateManager.cargoState = CARGOSTATE.OUT;
+        } else {
+            StateManager.cargoState = CARGOSTATE.INACTIVE;
         }
-
+        //Act according to state
         if (StateManager.cargoState == CARGOSTATE.IN && !RobotMap.mManipulator.isCargoIn()) {
           RobotMap.mManipulator.cargoIntake();
         } 
-
         if (StateManager.cargoState == CARGOSTATE.IN && RobotMap.mManipulator.isCargoIn()){
             StateManager.cargoState = CARGOSTATE.INACTIVE;
-            RobotMap.mManipulator.stopCargoWheels();
         }
-        
-        if (Controls.joystick.getRawButton(2)) {
+        if (StateManager.cargoState == CARGOSTATE.OUT) {
           RobotMap.mManipulator.cargoFire();
-        } else if (StateManager.cargoState == CARGOSTATE.INACTIVE) {
+        }
+        if (StateManager.cargoState == CARGOSTATE.INACTIVE) {
           RobotMap.mManipulator.stopCargoWheels();
         }
     
@@ -41,7 +45,6 @@ public class TeleopLoop {
           StateManager.targetAngle = 0;
         } else if (Controls.joystick.getRawButton(1)) {
           StateManager.targetAngle = ManipulatorConstants.lowerLimit - ManipulatorConstants.angleOffset + 10;
-          //10 is like ffwd but must be change for official use
         } else if (Controls.joystick.getRawButton(10)) {
           // StateManager.targetAngle = ArmConstants.upperLimit - ArmConstants.angleOffset;
           StateManager.targetAngle = 35;
